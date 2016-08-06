946a00b0c2289595ed1d1e09e0d9864291f8cfd2
huebe <huebe@huebe.com>
2016-08-06 15:09:16 +0200

    using bit array to store primes, not working yet

--- c_cuda/c_cuda/kernel.cu
+++ c_cuda/c_cuda/kernel.cu
@@ -5,35 +5,62 @@
 #include <stdio.h>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
+
+#define SET_BIT_CHAR_ARRAY(arr, i) ( arr[i/8] |= 1 << (i % 8) )
+#define CLEAR_BIT_CHAR_ARRAY(arr, i) ( arr[i/8] &= ~(char)(1 << (i % 8)) )
+#define READ_BIT_CHAR_ARRAY(arr, i) ( arr[i/8] & 1 << (i % 8) )
+
 
 //WARNING, has to be (x + 1) % cMaxThreads == 0
 const int cMaxThreads = 1024;
 const int cMax = 2047;
 const int cNumBlocks = (cMax + 1) / cMaxThreads;
+const int cResults = cMax / 2 + 1;
+const int cResultCharArraySize = cResults / 8;
+
+/*
+#if (((cMax + 1) % cMaxThreads ) != 0)
+	#pragma error
+#endif */
 
 // memory layout: index * 2 + 1
-__global__ void calculatePrimeSingle(int *isPrime) 
+__global__ void calculatePrimeSingle(char *isPrime) 
 {
 	int x = threadIdx.x;
 	int y = blockIdx.x;
 	int i = blockDim.x * y + x;
 	int numToCheck = i * 2 + 1;
-	isPrime[i] = true;
-	for (int j = 3; j < numToCheck && isPrime[i]; j++) {
+	SET_BIT_CHAR_ARRAY(isPrime, i);
+	for (int j = 3; j < (numToCheck / 2) && READ_BIT_CHAR_ARRAY(isPrime, i); j += 2) {
 		if ((numToCheck % j) == 0) {
-			isPrime[i] = false;
+			CLEAR_BIT_CHAR_ARRAY(isPrime, i);
 		}
 	}
-
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t calculatePrimeCuda(int *isPrime)
+__global__ void clearAll(char *isPrime)
 {
-    int *dev_isPrime = 0;
-    cudaError_t cudaStatus;
+	int x = threadIdx.x;
+	int y = blockIdx.x;
+	int i = blockDim.x * y + x;
+	CLEAR_BIT_CHAR_ARRAY(isPrime, i);
+}
 
-	// calculate kernel dimensions
+__global__ void setAll(char *isPrime)
+{
+	int x = threadIdx.x;
+	int y = blockIdx.x;
+	int i = blockDim.x * y + x;
+	SET_BIT_CHAR_ARRAY(isPrime, i);
+}
+
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t calculatePrimeCuda(char *isPrime)
+{
+    char *dev_isPrime = 0;
+    cudaError_t cudaStatus;
 
 
     // Choose which GPU to run on, change this on a multi-GPU system.
@@ -52,7 +79,7 @@ cudaError_t calculatePrimeCuda(int *isPrime)
 
 
     // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_isPrime, isPrime, cMax * sizeof(int), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_isPrime, isPrime, cResultCharArraySize, cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -60,7 +87,10 @@ cudaError_t calculatePrimeCuda(int *isPrime)
 
 
     // Launch a kernel on the GPU with one thread for each element.
-	calculatePrimeSingle <<<cNumBlocks, cMaxThreads >> >(dev_isPrime);
+	//calculatePrimeSingle <<<cNumBlocks, cMaxThreads >> >(dev_isPrime);
+
+	clearAll << <cNumBlocks, cMaxThreads >> >(dev_isPrime);
+	//setAll << <cNumBlocks, cMaxThreads >> >(dev_isPrime);
 
     // Check for any errors launching the kernel
     cudaStatus = cudaGetLastError();
@@ -78,7 +108,7 @@ cudaError_t calculatePrimeCuda(int *isPrime)
     }
 
     // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(isPrime, dev_isPrime, cMax * sizeof(int), cudaMemcpyDeviceToHost);
+	cudaStatus = cudaMemcpy(isPrime, dev_isPrime, cResultCharArraySize, cudaMemcpyDeviceToHost);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -93,7 +123,8 @@ Error:
 
 int main()
 {
-	int *cIsPrime = (int *)malloc((cMax / 2 + 1) * sizeof(int));
+	char *cIsPrime = (char *)malloc(cResultCharArraySize);
+	memset((void*)cIsPrime, 0x00, cResultCharArraySize);
 
 	clock_t tStart = clock();
 	cudaError_t cudaStatus = calculatePrimeCuda(cIsPrime);
@@ -104,13 +135,16 @@ int main()
 		return 1;
 	}
 
-	for (int i = 0; i < (cMax / 2 + 1); i++) {
-		if (cIsPrime[i]) {
+	int numPrimeNumbers = 0;
+	for (int i = 0; i < cResults; i++) {
+		if (READ_BIT_CHAR_ARRAY(cIsPrime, i)) {
 			printf("%i, ", i * 2 + 1);
+			numPrimeNumbers++;
 		}
 	}
 
-	printf("are prime numbers. Elapsed: %f seconds\n", (double)(tEnd - tStart) / CLOCKS_PER_SEC);
+	printf("are prime numbers.\nTotal %i prime numbers.\nElapsed: %f seconds\n", numPrimeNumbers, (double)(tEnd - tStart) / CLOCKS_PER_SEC);
+	free(cIsPrime);
 	
 	// cudaDeviceReset must be called before exiting in order for profiling and
 	// tracing tools such as Nsight and Visual Profiler to show complete traces.
@@ -120,7 +154,7 @@ int main()
 		return 1;
 	}
 
-	//free(cIsPrime);
+	
 	getchar();
 
 	return 0;
