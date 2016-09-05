b7a6a775d211e6f295fe07eaa1e402c5162b4ba1
huebe <huebe@huebe.com>
2016-09-05 11:50:38 +0200

    speed up with just check to sqrt(i)

--- c_basic/main.c
+++ c_basic/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef int bool;
 enum {
   false, true
 };
 
-const int cMax = 1000;
+const int cMax = 100000;
 
 void searchPrimes(unsigned int max, bool *isPrime) {
   //sanity checks and init stuff
@@ -23,6 +24,27 @@ void searchPrimes(unsigned int max, bool *isPrime) {
   //lets search for prime numbers
   for (int i = 3; i <= max; i += 2) {
     for (int j = 3; j < i / 2 && isPrime[i]; j += 2) {
+      if ((i % j) == 0) {
+        isPrime[i] = false;
+      }
+    }
+  }
+}
+
+void searchPrimes2(unsigned int max, bool *isPrime) {
+  //sanity checks and init stuff
+  if (max == 0) return;
+  isPrime[0] = false;
+  isPrime[1] = false;
+  if (max < 2) return;
+  isPrime[2] = true; //special case
+  isPrime[3] = true;
+  for (int i = 4; i <= max;
+       isPrime[i] = (i % 2 == 1) ? true : false, i++); //even numbers > 2 are NEVER prime numbers, ods can be
+
+  //lets search for prime numbers
+  for (int i = 3; i <= max; i += 2) {
+    for (int j = 3; j < sqrt(i) && isPrime[i]; j += 2) {
       if ((i % j) == 0) {
         isPrime[i] = false;
       }
