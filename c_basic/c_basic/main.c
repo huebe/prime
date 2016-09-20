2b045b4359e7b674cd3cf8c52682a00d3d70db01
Stefan Huber <super.huebe@gmail.com>
2016-09-20 19:50:57 +0200

    faster sqrt version fixed

--- c_basic/c_basic/main.c
+++ c_basic/c_basic/main.c
@@ -26,29 +26,29 @@ void searchPrimesBasic(unsigned long long max, unsigned long long *isPrime) {
   }
 }
 
-/* void searchPrimes2(unsigned long long max, bool *isPrime) {
+void searchPrimesBasic2(unsigned long long max, unsigned long long *isPrime) {
   //sanity checks and init stuff
   if (max == 0) return;
-  isPrime[0] = false;
-  isPrime[1] = false;
+  CLEAR_BIT_ULONGLONG_ARRAY(isPrime, 0);
+  CLEAR_BIT_ULONGLONG_ARRAY(isPrime, 1);
   if (max < 2) return;
-  isPrime[2] = true; //special case
-  isPrime[3] = true;
-  for (int i = 4; i <= max;
-       isPrime[i] = (i % 2 == 1) ? true : false, i++); //even numbers > 2 are NEVER prime numbers, ods can be
+  SET_BIT_ULONGLONG_ARRAY(isPrime, 2);
+  SET_BIT_ULONGLONG_ARRAY(isPrime, 3);
+  for (int i = 4; i <= max; ((i % 2 == 1) ? SET_BIT_ULONGLONG_ARRAY(isPrime, i) : CLEAR_BIT_ULONGLONG_ARRAY(isPrime, i)), i++);
 
   //lets search for prime numbers
   for (unsigned long long i = 3; i <= max; i += 2) {
-    for (unsigned long long j = 3; j <= sqrt(i) && isPrime[i]; j += 2) {
+    for (unsigned long long j = 3; j <= sqrt(i) && READ_BIT_ULONGLONG_ARRAY(isPrime, i); j += 2) {
       if ((i % j) == 0) {
-        isPrime[i] = false;
+        CLEAR_BIT_ULONGLONG_ARRAY(isPrime, i);
       }
     }
   }
-} */
+}
+
 
 void searchAndPrint(void (*searchPrimes)(unsigned long long, unsigned long long*), unsigned long long max) {
-  unsigned long long *isPrime = (unsigned long long *) malloc((max / ULONGLONG_SIZE_BITS) + 1);
+  unsigned long long *isPrime = (unsigned long long *) malloc(((max / ULONGLONG_SIZE_BITS) + 1) * sizeof(unsigned long long));
 
   clock_t tStart = clock();
   (*searchPrimes)(max, isPrime);
@@ -61,15 +61,14 @@ void searchAndPrint(void (*searchPrimes)(unsigned long long, unsigned long long*
       numPrimeNumbers++;
     }
   }
+
   printf("are prime numbers.\nTotal %i prime numbers.\nElapsed: %f seconds\n", numPrimeNumbers, (double)(tEnd - tStart) / CLOCKS_PER_SEC);
   free(isPrime);
 }
 
 int main() {
   searchAndPrint(&searchPrimesBasic, cMax);
-  //searchAndPrint(&searchPrimes2, cMax);
-  getchar();
-
+  searchAndPrint(&searchPrimesBasic2, cMax);
 
   return 0;
 }
